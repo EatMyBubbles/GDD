9fda42701cad0120689df4373846e2eee6b00551
Steven Zhao <szhao2399@gmail.com>
2015-07-21 11:34:11 -0400

    committing b/c luke told me to.

--- PongBall2.spritebuilder/Source/MainScene.swift
+++ PongBall2.spritebuilder/Source/MainScene.swift
@@ -1,37 +1,101 @@
 import Foundation
 
-class MainScene: CCNode {
+class MainScene: CCNode, CCPhysicsCollisionDelegate {
     
-    weak var Ball: CCSprite!
+    weak var ball: CCSprite!
     weak var rightPaddle: Paddle!
     weak var leftPaddle: Paddle!
     weak var gamePhysicsNode: CCPhysicsNode!
     
     var width = CCDirector.sharedDirector().viewSize().width
+    var gameOver: Bool = false
+//    var speed: Double = 1
+    var ballSpeed = 1
+    
+    
     
     func didLoadFromCCB() {
-        self.multipleTouchEnabled = true
-        //multipleTouchEnabled = true
+        multipleTouchEnabled = true
         userInteractionEnabled = true
+        gamePhysicsNode.collisionDelegate = self
+        
+        ballPush()
+//        physicsWorld.speed = ballSpeed
+        
+    }
+    
+    func ballPush() {
+        var ballX = CGFloat(arc4random() % 2)
+        var ballY = CGFloat(arc4random() % 2)
+        
+        if ballX == 0 {
+            ball.physicsBody.velocity.x = 100
+        } else {
+            ball.physicsBody.velocity.x = -100
+        }
+        
+        if ballY == 0 {
+            ball.physicsBody.velocity.y = 50
+        } else {
+            ball.physicsBody.velocity.y = -50
+        }
+    }
+    
+    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, ball nodeA: CCNode!, paddle nodeB: CCNode!) -> Bool {
+        
+        
+            leftPaddle.physicsBody.elasticity += 0.1
+            rightPaddle.physicsBody.elasticity += 0.1
+            ball.physicsBody.elasticity += 0.1
+       
+        
+        println(ball.physicsBody.density)
+        println(ball.physicsBody.friction)
+//
+//        ball.physicsBody.velocity.x = ( ballX * CGFloat (speed))
+//        ball.physicsBody.velocity.y = ( ballY * CGFloat (speed))
+        
+        return true
+    }
+    
+    override func update(delta: CCTime) {
+
+        if ball.position.x > width && gameOver == false {
+            println("GameOver")
+            gameOver = true
+        }
+        else if ball.position.x < 0 && gameOver == false {
+            println("GameOver")
+            gameOver = true
+        }
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-        var touchLocationY = touch.locationInNode(CCPhysicsNode()!).y
         
-//        println(touchLocationY)
-    }
-    
-    
-    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        var touchY = touch.locationInNode(CCPhysicsNode()!).y
         var touchX = touch.locationInNode(CCPhysicsNode()!).x
         
         if touchX > width/2 {
-            rightPaddle.positionInPoints.y = touch.locationInNode(CCPhysicsNode()!).y
+            rightPaddle.positionInPoints.y = touchY
         }
         else {
-            leftPaddle.positionInPoints.y = touch.locationInNode(CCPhysicsNode()!).y
+            leftPaddle.positionInPoints.y = touchY
         }
         
-        println(touch.locationInNode(CCPhysicsNode()!).y)
+    }
+    
+   
+    
+    override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        var touchY = touch.locationInNode(CCPhysicsNode()!).y
+        var touchX = touch.locationInNode(CCPhysicsNode()!).x
+        
+        if touchX > width/2 {
+            rightPaddle.positionInPoints.y = touchY
+        }
+        else {
+            leftPaddle.positionInPoints.y = touchY
+        }
+        
     }
 }
