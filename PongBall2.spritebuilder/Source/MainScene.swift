187729f06609b7537f65498cc9bb41b8c6f3a171
Steven Zhao <szhao2399@gmail.com>
2015-07-22 15:35:21 -0400

    committing bcuz luke is telepathically telling me to do it.

--- PongBall2.spritebuilder/Source/MainScene.swift
+++ PongBall2.spritebuilder/Source/MainScene.swift
@@ -1,15 +1,15 @@
 import Foundation
-
+//note to self: get the right friction amount
 class MainScene: CCNode, CCPhysicsCollisionDelegate {
     
     weak var ball: CCSprite!
     weak var rightPaddle: Paddle!
     weak var leftPaddle: Paddle!
     weak var gamePhysicsNode: CCPhysicsNode!
+    weak var restartButton: CCButton!
     
     var width = CCDirector.sharedDirector().viewSize().width
     var gameOver: Bool = false
-//    var speed: Double = 1
     var ballSpeed = 1
     
     
@@ -20,7 +20,6 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
         gamePhysicsNode.collisionDelegate = self
         
         ballPush()
-//        physicsWorld.speed = ballSpeed
         
     }
     
@@ -29,45 +28,51 @@ class MainScene: CCNode, CCPhysicsCollisionDelegate {
         var ballY = CGFloat(arc4random() % 2)
         
         if ballX == 0 {
-            ball.physicsBody.velocity.x = 100
+            ball.physicsBody.velocity.x = 150
         } else {
-            ball.physicsBody.velocity.x = -100
+            ball.physicsBody.velocity.x = -150
         }
         
         if ballY == 0 {
-            ball.physicsBody.velocity.y = 50
+            ball.physicsBody.velocity.y = 75
         } else {
-            ball.physicsBody.velocity.y = -50
+            ball.physicsBody.velocity.y = -75
         }
     }
     
-    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, ball nodeA: CCNode!, paddle nodeB: CCNode!) -> Bool {
+    func ccPhysicsCollisionBegin(pair: CCPhysicsCollisionPair!, ball nodeA: CCNode!, paddle nodeB: CCNode!) -> ObjCBool {
         
-        
-            leftPaddle.physicsBody.elasticity += 0.1
-            rightPaddle.physicsBody.elasticity += 0.1
+        if ball.physicsBody.elasticity < 1.1 && ball.physicsBody.velocity.x < 950 && ball.physicsBody.velocity.x > -950 {
             ball.physicsBody.elasticity += 0.1
-       
-        
-        println(ball.physicsBody.density)
-        println(ball.physicsBody.friction)
-//
-//        ball.physicsBody.velocity.x = ( ballX * CGFloat (speed))
-//        ball.physicsBody.velocity.y = ( ballY * CGFloat (speed))
-        
+        } else if ball.physicsBody.elasticity == 1.1 {
+            ball.physicsBody.elasticity -= 0.1
+        }
+
+        println(ball.physicsBody.velocity)
+
         return true
+    }
+    
+    func restart () {
+        let mainScene = CCBReader.loadAsScene("MainScene")
+        CCDirector.sharedDirector().replaceScene(mainScene)
     }
     
     override func update(delta: CCTime) {
 
         if ball.position.x > width && gameOver == false {
             println("GameOver")
+            restartButton.visible = true
             gameOver = true
         }
         else if ball.position.x < 0 && gameOver == false {
             println("GameOver")
+            restartButton.visible = true
             gameOver = true
         }
+        
+        let velocityX = clampf(Float(ball.physicsBody.velocity.x), -Float(CGFloat.max),950)
+        ball.physicsBody.velocity = ccp(CGFloat(velocityX), CGFloat(ball.physicsBody.velocity.y))
     }
     
     override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
